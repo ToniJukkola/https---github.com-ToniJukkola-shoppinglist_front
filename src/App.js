527dab009ae54d843fc0b8ff8ed2e151b6d290d7
ToniJukkola <n1juto00@students.oamk.fi>
2022-03-16 22:17:08 +0200

    Tietokannasta saadaan tietoa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const URL = 'http://localhost/shoppinglist';
+const URL = 'http://localhost/shoppinglist/';
+
 
 function App() {
-  
+  const [items, setItems] = useState([]);
+
+useEffect(() => {
+  axios.get(URL)
+  .then((response) => {
+    //console.log(response.data);
+    setItems(response.data)
+  }).catch(error => {
+    alert(error);
+  })
+
+}, [])
+
   return (
     
-    <div className="App">
+    <div>
       
     </div>
   );
