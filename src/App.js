f458b9ab21adb699a2e73cbb2ce7b62400ce1e95
ToniJukkola <n1juto00@students.oamk.fi>
2022-03-17 20:14:46 +0200

    Tietoja voi nyt tallettaa tietokantaan

--- src/App.js
+++ src/App.js
@@ -6,31 +6,51 @@ const URL = 'http://localhost/shoppinglist/';
 
 
 function App() {
+  const [item, setItem] = useState('');
   const [items, setItems] = useState([]);
 
 useEffect(() => {
   axios.get(URL)
   .then((response) => {
-    //console.log(response.data);
     setItems(response.data)
   }).catch(error => {
-    alert(error);
-  })
-
+    alert(error.response ? error.response.data.error : error);
+    });
 }, [])
+
+
+function add(e) {
+  e.preventDefault();
+  const json = JSON.stringify({description:item});
+  axios.post(URL + 'add.php',json, {
+    headers: {
+      'Content-Type' : 'application/json'
+    }
+  })
+  .then((response) => {
+    setItems(items => [...items,response.data]);
+    setItem('');
+  }).catch(error => {
+    alert(error.response ? error.response.data.error : error);
+  })
+}
+
 
   return (
     
     <div>
+      <form onSubmit={add}>
       <h3>Shopping list</h3>
-      <input></input>
-      <input type="number"></input>
-      <input type="button" value="Add"></input>
+      <input value={item} placeholder="Type description" type="text" onChange={e => setItem(e.target.value)}></input>
+      <input placeholder="Type amount" type="number"></input>
+      <button>Add</button>
+      </form>
       <ul>
-        {items?.map(task =>(
-          <li key={items.id}>{task.description}</li>
+        {items?.map(item =>(
+          <li key={item.id}>{item.description}{" "}{item.amount}</li>
         ))}
       </ul>
+      
     </div>
   );
 }
