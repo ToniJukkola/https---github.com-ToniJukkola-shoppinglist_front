136dedcc3000addb76e53ba4a5547c52d72d1634
ToniJukkola <n1juto00@students.oamk.fi>
2022-03-18 15:26:31 +0200

    descriptionin lisäys toimii

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './index.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -8,6 +9,7 @@ const URL = 'http://localhost/shoppinglist/';
 function App() {
   const [item, setItem] = useState('');
   const [items, setItems] = useState([]);
+  const [amount, setAmount] = useState([]);
 
 useEffect(() => {
   axios.get(URL)
@@ -21,7 +23,7 @@ useEffect(() => {
 
 function add(e) {
   e.preventDefault();
-  const json = JSON.stringify({description:item});
+  const json = JSON.stringify({description:item, amount:item});
   axios.post(URL + 'add.php',json, {
     headers: {
       'Content-Type' : 'application/json'
@@ -30,6 +32,8 @@ function add(e) {
   .then((response) => {
     setItems(items => [...items,response.data]);
     setItem('');
+    setAmount(amount => [...amount,response.data]);
+    setAmount('');
   }).catch(error => {
     alert(error.response ? error.response.data.error : error);
   })
@@ -42,7 +46,7 @@ function add(e) {
       <form onSubmit={add}>
       <h3>Shopping list</h3>
       <input value={item} placeholder="Type description" type="text" onChange={e => setItem(e.target.value)}></input>
-      <input placeholder="Type amount" type="number"></input>
+      <input value={amount} placeholder="Type amount" type="text" onChange={e => setAmount(e.target.value)}></input>
       <button>Add</button>
       </form>
       <ul>
